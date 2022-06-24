b58795f8a0b4a30cdd53f83b216879fae373712f
Valts Vilcans <valtsvilcans.dev@gmail.com>
2022-06-24 17:20:37 +0100

    Finished Ejercicio 1

---
+++ README.md
@@ -0,0 +1,8 @@
+#Ejercicios de curso C#
+
+### Video 1
+#### Ejercicio 1
+Escribe un programma que reciba tu nombre y lo escriba por consola.
+
+#### Ejercicio 2
+Escribe un programa que tome la hora y la escriba por pantalla.
---
+++ Video1Ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿double num1 = 0;
+while (true)
+{
+    Console.WriteLine("Enter your name: ");
+    string userInput = Console.ReadLine();
+
+
+    if (double.TryParse(userInput, out num1)) {
+        // if the parse was successful, we can break out of the loop
+        Console.WriteLine("Can't have a number for a name!!! This is not year 2098!");
+    }
+    else
+    {
+        // if the parse was unsuccessful, display an error message and try again
+        Console.WriteLine("Your name is: " + userInput);
+        break;
+    }
+}
+
