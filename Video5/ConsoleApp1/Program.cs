dabe2d28cf16637ed835db5fdc159ac120384276
Valts Vilcans <valtsvilcans.dev@gmail.com>
2022-06-26 16:19:07 +0100

    Ejercicios 1,2 Video 5

---
+++ Video5/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿double price = 24.99;
+
+Console.WriteLine("Enter name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Enter email: ");
+string email = Console.ReadLine();
+Console.WriteLine("Enter cupon code: ");
+string cuponCode = Console.ReadLine();
+
+double reduction = Convert.ToInt32(cuponCode.Substring(cuponCode.Length - 2));
+
+double newPrice = price - (reduction / 100 * price);
+
+Console.WriteLine($"{name} {email} {price} {Math.Round(newPrice, 2)}");
---
+++ Video5/Ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿List<string> list = new List<string> { "JS", "C#", "Java", "C", "Python" };
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Choose language from 1 to 5");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine(list[0]);
+        break;
+    case 2:
+        Console.WriteLine(list[1]);
+    break;
+    case 3:
+        Console.WriteLine(list[2]);
+    break;
+    case 4:
+        Console.WriteLine(list[3]);
+    break;
+    case 5:
+        Console.WriteLine(list[4]);
+    break;
+
+    default:
+        break;
+}
